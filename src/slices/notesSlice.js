2bc4ab9524c2fc123115df2ae5de39d129b166a4
Prince Sharma <princesharma4867@gmail.com>
2025-10-05 10:41:59 +0530

    initial commit

---
+++ src/slices/notesSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+initialState={
+    notes:[]
+}
+
+const notesSlice = createSlice({
+    name:"note",
+    initialState,
+    reducers:{
+        getnotes:
+    }
+})
